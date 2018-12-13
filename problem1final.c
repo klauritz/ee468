2e5070e1c64f7fe9439aa14063ba5cf8baf20ff8
klauritz <klauritz@hawaii.edu>
2018-12-13 13:14:16 -1000

    Create problem1final.c

---
+++ problem1final.c
@@ -0,0 +1,19 @@
+int main( ) {
+    pid_t pid;
+    int status = 0;
+    int i;
+    for (i = 0; i < 100; i++) {
+        pid = fork( );
+        if (pid < 0) {
+            fprintf(stderr, "Fork Failed");
+            return 1;
+        }
+        else if (pid == 0) {
+            printf("I am child # %d\n", i);
+            return 0;
+        }
+    }
+    while ((pid = wait(&status)) > 0);
+    printf("All child processes are done \n");
+    return 0;
+} /* end of main */
